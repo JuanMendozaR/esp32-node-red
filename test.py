29cd1126d98b26c12e254c45f8b603c7a70139a1
JuanDe Mendoza <106559817+JuanMendozaR@users.noreply.github.com>
2024-08-30 21:48:55 -0600

    Update test.py

--- test.py
+++ test.py
@@ -1,9 +1,9 @@
 import cv2
 
-vid = cv2.VideoCapture("rtsp:192.168.1.68:8080/h264_ulaw.sdp")
+vid = cv2.VideoCapture("rtsp:192.168.x.x:8080/h264_ulaw.sdp")
 
 
-while(vid.isOpened()):
+while(vid.isOpened())
     ret,image = vid.read()
     if ret == True:
         resized = cv2.resize(image, (450, 360), interpolation=cv2.INTER_AREA)
